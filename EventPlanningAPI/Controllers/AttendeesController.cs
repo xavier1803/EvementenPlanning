0c5a9a3feff10b0eab657843542440b273bef042
xavier1803 <56248472+xavier1803@users.noreply.github.com>
2023-10-20 11:14:46 +0200

    Adds API controllers

---
+++ EventPlanningAPI/Controllers/AttendeesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EvementenPlanning.Models;
+
+namespace EventPlanningAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendeesController : ControllerBase
+    {
+        private readonly EventplanningContext _context;
+
+        public AttendeesController(EventplanningContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Attendees
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Attendee>>> GetAttendees()
+        {
+          if (_context.Attendees == null)
+          {
+              return NotFound();
+          }
+            return await _context.Attendees.ToListAsync();
+        }
+
+        // GET: api/Attendees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Attendee>> GetAttendee(int id)
+        {
+          if (_context.Attendees == null)
+          {
+              return NotFound();
+          }
+            var attendee = await _context.Attendees.FindAsync(id);
+
+            if (attendee == null)
+            {
+                return NotFound();
+            }
+
+            return attendee;
+        }
+
+        // PUT: api/Attendees/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAttendee(int id, Attendee attendee)
+        {
+            if (id != attendee.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(attendee).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AttendeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Attendees
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Attendee>> PostAttendee(Attendee attendee)
+        {
+          if (_context.Attendees == null)
+          {
+              return Problem("Entity set 'EventplanningContext.Attendees'  is null.");
+          }
+            _context.Attendees.Add(attendee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAttendee", new { id = attendee.Id }, attendee);
+        }
+
+        // DELETE: api/Attendees/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAttendee(int id)
+        {
+            if (_context.Attendees == null)
+            {
+                return NotFound();
+            }
+            var attendee = await _context.Attendees.FindAsync(id);
+            if (attendee == null)
+            {
+                return NotFound();
+            }
+
+            _context.Attendees.Remove(attendee);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AttendeeExists(int id)
+        {
+            return (_context.Attendees?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ EventPlanningAPI/Controllers/OrganizersController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EvementenPlanning.Models;
+using EvementenPlanning.Models.Classes;
+
+namespace EventPlanningAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizersController : ControllerBase
+    {
+        private readonly EventplanningContext _context;
+
+        public OrganizersController(EventplanningContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Organizers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Organizer>>> GetOrganizers()
+        {
+          if (_context.Organizers == null)
+          {
+              return NotFound();
+          }
+            return await _context.Organizers.ToListAsync();
+        }
+
+        // GET: api/Organizers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Organizer>> GetOrganizer(int id)
+        {
+          if (_context.Organizers == null)
+          {
+              return NotFound();
+          }
+            var organizer = await _context.Organizers.FindAsync(id);
+
+            if (organizer == null)
+            {
+                return NotFound();
+            }
+
+            return organizer;
+        }
+
+        // PUT: api/Organizers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrganizer(int id, Organizer organizer)
+        {
+            if (id != organizer.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(organizer).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrganizerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Organizers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Organizer>> PostOrganizer(Organizer organizer)
+        {
+          if (_context.Organizers == null)
+          {
+              return Problem("Entity set 'EventplanningContext.Organizers'  is null.");
+          }
+            _context.Organizers.Add(organizer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrganizer", new { id = organizer.Id }, organizer);
+        }
+
+        // DELETE: api/Organizers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrganizer(int id)
+        {
+            if (_context.Organizers == null)
+            {
+                return NotFound();
+            }
+            var organizer = await _context.Organizers.FindAsync(id);
+            if (organizer == null)
+            {
+                return NotFound();
+            }
+
+            _context.Organizers.Remove(organizer);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool OrganizerExists(int id)
+        {
+            return (_context.Organizers?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
